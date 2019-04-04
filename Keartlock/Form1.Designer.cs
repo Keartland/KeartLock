fecfbb0c21963a4a2f667f84b86ea3b314b7ed5d
Harry Keartland <harrykeartland@gmail.com>
2019-04-04 21:22:52 +0100

    Add files via upload

---
+++ Keartlock/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Keartlock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 114);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mate basically this locks \r\nyour mouse to the primary \r\nmonitor and sets counter " +
+    "\r\nstrike and discord to high \r\npriorty, not that deep.\r\n\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(254, 101);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(270, 140);
+            this.MinimumSize = new System.Drawing.Size(270, 140);
+            this.Name = "Form1";
+            this.Text = "KeartLock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Keartlock/Form1.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Keartlock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            highPriority();
+        }
+
+        private System.Windows.Forms.Timer timer1;
+        public void highPriority()
+        {
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 2000; // in miliseconds
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int height = SystemInformation.PrimaryMonitorSize.Height;
+            int width = SystemInformation.PrimaryMonitorSize.Width;
+            Cursor.Clip = new Rectangle(new Point(0, 0), new Size(width, height));
+            foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses().Where(x => x.ProcessName == "Discord"))
+            {
+                p.PriorityClass = System.Diagnostics.ProcessPriorityClass.High;
+            }
+            foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses().Where(x => x.ProcessName == "csgo"))
+            {
+                p.PriorityClass = System.Diagnostics.ProcessPriorityClass.High;
+            }
+        }
+    }
+}
